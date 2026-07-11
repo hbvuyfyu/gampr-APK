afad21aa770a3b420e9f5de86c0dc827ea107af3
hbvuyfyu <janeblakely1@outlook.com>
2026-07-11 17:02:03 +0300

    Update app_config.dart

--- mobile/lib/config/app_config.dart
+++ mobile/lib/config/app_config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   // ضع رابط الـ Backend هنا (Railway أو أي سيرفر آخر)
   // مثال: 'https://your-app.railway.app'
-  static const String baseUrl = 'https://gampr-apk-production.up.railway.app';
+  static const String baseUrl = 'https://gampar-apk-production.up.railway.app';
   static const String apiUrl = '$baseUrl/api';
 }
